05631dbccb7761023ee2d8c855e8f86247f01539
gh0st1608 <egalindoa@uni.pe>
2024-09-26 04:07:08 -0500

    fix region variable

--- stack2/provider.tf
+++ stack2/provider.tf
@@ -12,7 +12,7 @@ terraform {
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region     = var.region
+  region     = "us-east-1"
 
 /*   s3_use_path_style         = true
   skip_credentials_validation = true
