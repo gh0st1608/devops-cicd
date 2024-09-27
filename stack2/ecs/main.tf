0df5ab74f075eb3f534ccc4c23796bbeabcd5114
gh0st1608 <egalindoa@uni.pe>
2024-09-27 12:07:49 -0500

    fix: add desired count 1

--- stack2/ecs/main.tf
+++ stack2/ecs/main.tf
@@ -16,6 +16,7 @@ resource "aws_ecs_task_definition" "django_task" {
     name      = "django_container"
     image     = "${var.ecs_image_name}:latest"
     essential = true
+    
     portMappings = [{
       containerPort = 8000
       hostPort      = 8000
@@ -28,6 +29,7 @@ resource "aws_ecs_service" "django_service" {
   cluster         = aws_ecs_cluster.django_cluster.id
   task_definition = aws_ecs_task_definition.django_task.id
   launch_type     = "FARGATE"
+  desired_count   = 1
 
   network_configuration {
     subnets          = ["subnet-0a1d412d9012fd6f6"]  # Reemplaza con tu subred
